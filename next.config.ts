b12beee1a7521b4a4c9cab91b50713b74c4281d9
AioTodo <anasslana2004@gmail.com>
2025-02-13 13:41:54 +0100

    Update GitHub Actions workflow

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  },
-  basePath: '/inbites', // Replace with your actual repository name
-  assetPrefix: '/inbites', // Replace with your actual repository name
+  }
 };
 
 export default nextConfig;
